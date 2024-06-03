2ad8f4a89d9ab19ac9b1a814707e1e30db0e118c
Shahzod <shahzodwill@gmail.com>
2024-06-03 21:29:16 +0200

    changed the path

--- js/transferID.mjs
+++ js/transferID.mjs
@@ -1,3 +1,4 @@
+import { repo } from "./posts/constants.mjs";
 export function transferPostID(){
     const elements = document.querySelectorAll('.blog-item');
     const readMore = document.querySelector('.readMore');
@@ -6,7 +7,7 @@ export function transferPostID(){
         element.addEventListener('click', (event)=> {
             event.preventDefault();
             const id = element.getAttribute('id');
-            if (window.location.pathname === '/'){
+            if (window.location.pathname === `${repo}/` || window.location.pathname === `${repo}/index.html` || window.location.pathname === '/' || window.location.pathname === '/index.html'){
                 window.location.href = `post/index.html?id=${id}&page=${window.location.pathname}`;
             } else if (window.location.pathname === '/post/articles.html') {
                 window.location.href = `index.html?id=${id}&page=${window.location.pathname}`;
